88f57fd833021f5c552c56c518d8e3960bd088fa
andrespxp <davidmonterrosa52@gmail.com>
2021-06-05 03:33:00 -0500

    ayuda codigo notas

---
+++ index.php
@@ -0,0 +1,12 @@
+<?php
+include_once("./include/header.php");
+?>
+
+<p>  efrbgwehlsbtueuibveutblptionts dhbfavklehyl fe bfvaih eclasd 
+  eutbh´pwetnhb´setibnet esntb t b wptp bg ´pentp bpej psr b .eu8tr v9qwbrf 
+ euib oasyfo seoutbnpsuilo sbañdjkvNÁWUIBV´SDBN{ SDIFB+AEOMRBETPB,K  JBN ZBVFA
+ALUBFVÁSNDF´NAP PAN LADFV  PERTHOPN  ÑSDJF VLEIWGF´webv clñ} </p>
+
+<?php
+include_once("./include/footer.php");
+?>
